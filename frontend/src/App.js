5d557d5108b8f1c367e58e3d37943d6c8440b1f2
JatinnVaityy <vaityjatin13@gmail.com>
2025-02-28 22:09:05 +0530

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import Themeroutes from "./routes/Router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  // Define your routes
+  const routing = useRoutes(Themeroutes);
+
+  return (
+    <div className="dark">
+      {routing}
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default () => (
+  <Router basename="/LiveWell">
+    <App />
+  </Router>
+);
---
+++ frontend/src/index.js
@@ -0,0 +1,26 @@
+import React, { Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/scss/style.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter, BrowserRouter } from "react-router-dom";
+import Loader from "./layouts/loader/Loader";
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      <Suspense fallback={<Loader/>}>
+        <BrowserRouter basename="/LiveWell">
+          <App />
+        </BrowserRouter>
+      </Suspense>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found. Check your index.html.");
+}
+
+reportWebVitals();
