07b553d7e3aa02b241243e1c028cf7af47a08d1f
JatinnVaityy <vaityjatin13@gmail.com>
2025-02-28 20:29:28 +0530

    updatess

--- package/src/index.js
+++ package/src/index.js
@@ -1,21 +1,26 @@
 import React, { Suspense } from "react";
-// import ReactDOM from "react-dom";
-import {createRoot} from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import "./assets/scss/style.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
-import Loader from "./layouts/loader/Loader";
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+const rootElement = document.getElementById("root");
 
-root.render(
-  
-    <HashRouter>
-      <App />
-    </HashRouter>
+if (rootElement) {
+  const root = createRoot(rootElement);
 
-);
+  root.render(
+    <React.StrictMode>
+      <Suspense fallback={<div>Loading...</div>}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Suspense>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found. Check your index.html.");
+}
 
 reportWebVitals();
